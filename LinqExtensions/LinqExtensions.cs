ee5e6a77014d94f154cf2861b29f441bbee871ce
Igor Cervac <igor.cervac@outlook.com>
2024-01-22 19:01:07 -0500

    Initial

---
+++ LinqExtensions/LinqExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqApp
+{
+    static partial class LinqExtensions
+    {
+        public static U MaxBy<U, V>(this IEnumerable<U> source, Func<U, V> keySelector)
+            where V : IComparable<V>
+        {
+            return source.Aggregate((x, y) => keySelector(y).CompareTo(keySelector(x)) > 0 ? y : x);
+        }
+
+    }
+}
